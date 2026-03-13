f4ce138d38f4b9ade52321ae9b66cd7ef70d4e68
Burainzu <117584117+Burainzu@users.noreply.github.com>
2026-03-13 22:54:33 +0000

    Clearing unused code

--- cek_model.py
+++ cek_model.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 
 # Masukkan API key kamu langsung di sini untuk testing
-genai.configure(api_key="AIzaSyBviPE0HSBW35H2Euv-nNpODMyVBExuhk0")
+genai.configure(api_key="-")
 
 print("Model yang tersedia:")
 for m in genai.list_models():
